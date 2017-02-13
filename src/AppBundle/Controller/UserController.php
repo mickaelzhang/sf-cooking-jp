b70dbcf6a076f4dc28007c0ca05ef7b4c228ef13
Clément Vion <clement.vion49@gmail.com>
2017-02-13 14:56:59 +0100

    [ADD] retrieve users list

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -4,7 +4,29 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
+/**
+ * Class UserController
+ *
+ * @package AppBundle\Controller
+ * @Route("utilisateurs")
+ */
 class UserController extends Controller
 {
+    /**
+     * List all users
+     *
+     * @Route("/", name="users_list")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository('AppBundle:User')->findAll();
+
+        return $this->render('@frontend/users/list.html.twig', array(
+            'users' => $users
+        ));
+    }
 }
