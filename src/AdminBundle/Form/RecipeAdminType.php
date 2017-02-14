d7e6f5dee6f06493342da994b6eeab4213215c38
Clément Vion <clement.vion49@gmail.com>
2017-02-14 22:40:39 +0100

    [ADD] list of recipes and edit in admin

---
+++ src/AdminBundle/Form/RecipeAdminType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AdminBundle\Form;
+
+use AppBundle\Entity\Recipe;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+
+/**
+ * Class RecipeAdminType
+ * @package AppBundle\Form
+ */
+class RecipeAdminType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('difficulty', IntegerType::class)
+            ->add('servingSize', IntegerType::class)
+            ->add('preparationTime', TimeType::class, array(
+                'input' => 'datetime',
+                'widget' => 'single_text',
+            ))
+            ->add('cookingTime', TimeType::class, array(
+                'input' => 'datetime',
+                'widget' => 'single_text',
+            ))
+            ->add('instructions', TextareaType::class);
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Recipe::class,
+        ));
+    }
+}
