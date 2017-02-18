d99cd87b1187da66eaf38763442b00725080f484
Clément Vion <clement.vion49@gmail.com>
2017-02-18 16:27:22 +0100

    [ADD] styles inscriptions

--- src/FrontOfficeBundle/Form/UserType.php
+++ src/FrontOfficeBundle/Form/UserType.php
@@ -16,17 +16,56 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class)
-            ->add('firstname', TextType::class)
-            ->add('lastname', TextType::class)
-            ->add('username', TextType::class)
-            ->add('image', FileType::class, array(
-                'label' => 'Image de profil'
+            ->add('username', TextType::class, array(
+                'label' => ' ',
+                'attr' => array(
+                    'placeholder' => 'Mon pseudo*',
+                    'class' => 'forms__inputs forms__inputsFull',
+                ),
+            ))
+            ->add('email', EmailType::class, array(
+                'label' => ' ',
+                'attr' => array(
+                    'placeholder' => 'Mon email*',
+                    'class' => 'forms__inputs forms__inputsFull',
+                ),
             ))
             ->add('plainPassword', RepeatedType::class, array(
                 'type' => PasswordType::class,
-                'first_options'  => array('label' => 'Password'),
-                'second_options' => array('label' => 'Repeat Password'),
+                'first_options'  => array(
+                    'label' => ' ',
+                    'attr' => array(
+                        'placeholder' => 'Mon mot de passe',
+                        'class' => 'forms__inputs forms__inputsHalf',
+                    )
+                ),
+                'second_options' => array(
+                    'label' => ' ',
+                    'attr' => array(
+                        'placeholder' => 'Répéter le mot de passe',
+                        'class' => 'forms__inputs forms__inputsHalf',
+                    )
+                ),
+            ))
+            ->add('firstname', TextType::class, array(
+                'label' => ' ',
+                'attr' => array(
+                    'placeholder' => 'Mon prénom*',
+                    'class' => 'forms__inputs forms__inputsHalf',
+                ),
+            ))
+            ->add('lastname', TextType::class, array(
+                'label' => ' ',
+                'attr' => array(
+                    'placeholder' => 'Mon nom*',
+                    'class' => 'forms__inputs forms__inputsHalf',
+                ),
+            ))
+            ->add('image', FileType::class, array(
+                'label' => ' ',
+                'attr' => array(
+                    'class' => 'forms__inputs forms__inputsFull forms__inputsImage',
+                ),
             ))
         ;
     }
